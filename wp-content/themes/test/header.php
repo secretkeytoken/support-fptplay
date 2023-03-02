0c3669166ccd39ceacca8b01743bebd333055a40
thanhth22 <thanhth22@fpt.com.vn>
2023-03-02 16:59:54 +0700

    update ci/cd

--- wp-content/themes/test/header.php
+++ wp-content/themes/test/header.php
@@ -17,6 +17,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<?php wp_head(); ?>
 </head>
 
